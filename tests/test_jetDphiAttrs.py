413e6728cc38297a5ba918def409305865c81656
Tai Sakuma <tai.sakuma@gmail.com>
2018-01-20 15:16:57 +0000

    add test_no_jet()

--- tests/test_jetDphiAttrs.py
+++ tests/test_jetDphiAttrs.py
@@ -37,3 +37,43 @@ def test_one(tbl_scan_event, tbl_scan_jet, scribblers):
             assert pytest.approx(expected, abs = 1e-6) == actual
 
 ##__________________________________________________________________||
+def test_no_jet(scribblers):
+    event = mock.MagicMock()
+    event.jet_pt = [ ]
+    event.jet_phi = [ ]
+
+    for scribbler in scribblers:
+        scribbler.begin(event)
+
+    event.jet_pt[:] = [ ]
+    event.jet_phi[:] = [ ]
+
+    for scribbler in scribblers:
+        scribbler.event(event)
+
+    assert [0.0] == event.mht_jet40
+    assert [ ] == event.jet40_pt
+    assert [ ] == event.jet40_phi
+    assert [ ] == event.jet40_dphi
+    assert [ ] == event.jet40_dphiHat
+    assert [ ] == event.jet40_dphiTilde
+    assert [ ] == event.jet40_bDphi
+    assert [ ] == event.jet40_omega
+    assert [ ] == event.jet40_omegaHat
+    assert [ ] == event.jet40_omegaTilde
+    assert [ ] == event.jet40_chi
+    assert [ ] == event.jet40_f
+    assert [ ] == event.jet40_g
+    assert [ ] == event.jet40_k
+    assert [ ] == event.jet40_h
+    assert [ ] == event.jet40_arccotF
+
+    assert [-1] == event.minbDphi
+    assert [-1] == event.minOmega
+    assert [-1] == event.minOmegaHat
+    assert [-1] == event.minOmegaTilde
+    assert [-1] == event.minChi
+    ## assert [-1] == event.xi
+    assert [-1] == event.maxF
+
+##__________________________________________________________________||
