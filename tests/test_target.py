6986a4db8f11e2c786457660b4d03f6dcfe85850
Tai Sakuma <tai.sakuma@gmail.com>
2018-01-21 12:34:33 +0000

    update test_target.py

--- tests/test_target.py
+++ tests/test_target.py
@@ -1,20 +1,32 @@
 # Tai Sakuma <tai.sakuma@gmail.com>
 import numpy as np
 
+import pytest
+
 from .target import TargetAltDphi
 from altdphi import AltDphi
 
 ##__________________________________________________________________||
-def test_comp():
+def test_simple():
 
     pt = np.array([741.63,  498.69, 45.62])
     phi = np.array([-1.41,  1.81, 0.92])
     alt = AltDphi(pt=pt, phi=phi)
+
+    target = TargetAltDphi(pt=pt, phi=phi)
+
+    target.assert_equal(alt)
+
+def test_raise():
+
+    pt = np.array([741.63,  498.69, 45.62])
+    phi = np.array([-1.41,  1.81, 0.92])
     alt = AltDphi(pt=pt, phi=phi)
 
     phi = np.array([-1.41,  1.81, 0.93])
     target = TargetAltDphi(pt=pt, phi=phi)
 
-    target.assert_equal(alt)
+    with pytest.raises(AssertionError):
+        target.assert_equal(alt)
 
 ##__________________________________________________________________||
