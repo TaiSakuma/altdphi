8380be92c6ce77d2b3f745680d13875e583f4f64
Tai Sakuma <tai.sakuma@gmail.com>
2018-01-25 19:06:45 +0000

    update generate_expected.py

--- tests/generate_expected.py
+++ tests/generate_expected.py
@@ -27,62 +27,77 @@ expected_py_head = expected_py_head.lstrip()
 ##__________________________________________________________________||
 def main():
 
-    expected_str = compose_expected_str()
+    njet_name_dict = {
+        0: 'nojet',
+        1: 'monojet',
+        2: 'two_jets',
+        3: 'three_jets',
+        4: 'four_jets'
+    }
+
+    event_dict = correct_event_data(njet_name_dict)
+
+    expected_as_string = compose_expected_as_string(event_dict, njet_name_dict)
 
     print expected_py_head
-    print expected_str
+    print expected_as_string
 
-##__________________________________________________________________||
-def compose_expected_str():
+def correct_event_data(njet_name_dict):
 
     tbl_scan_event = tbl_scan_event_()
     tbl_scan_jet = tbl_scan_jet_()
 
-    njet_name_dict = {0: 'nojet', 1: 'monojet', 2: 'two_jets', 3: 'three_jets', 4: 'four_jets'}
-
     njet_list = sorted(njet_name_dict.keys())
-    # e.g., [0, 1, 2, 3, 4]
 
     event_dict = { }
 
-    ## collect
     njet_list_ = list(njet_list)
 
     # nojet
-    event_dict[0] = dict(pt=np.array([ ]), phi=np.array([ ]))
+    event_dict[0] = dict(pt=np.array([ ]), phi=np.array([ ]), met=10.12, met_phi=2.41)
     njet_list_.remove(0)
 
-    #
+    # 1 or more jets
     for evt in tbl_scan_event.index:
         tbl_event = tbl_scan_event.loc[evt]
         tbl_jet = tbl_scan_jet.loc[[evt]]
 
-        pt = tbl_jet.jet_pt.values
-        phi = tbl_jet.jet_phi.values
-
         njet = int(tbl_event.njet)
-        event_dict[njet] = dict(pt=pt, phi=phi)
-
         if njet not in njet_list_:
             continue
+
+        pt = tbl_jet.jet_pt.values
+        phi = tbl_jet.jet_phi.values
+        met = tbl_event.met_pt
+        met_phi = tbl_event.met_phi
+        event_dict[njet] = dict(pt=pt, phi=phi, met=met, met_phi=met_phi)
 
         njet_list_.remove(njet)
         if not njet_list_:
             break
 
-    # print
+    return event_dict
+
+def compose_expected_as_string(event_dict, njet_name_dict):
+
+    njet_list = sorted(event_dict.keys())
+
     contents = [ ]
     for njet in njet_list:
         njet_name = njet_name_dict[njet]
         event = event_dict[njet]
         pt = event['pt']
         phi = event['phi']
+        met = event['met']
+        met_phi = event['met_phi']
         alt = AltDphi(pt=pt, phi=phi)
         dumped = dump_altdphi(alt)
         lines = ''
         lines = lines + '{} = dict(\n'.format(njet_name)
         lines = lines + '    pt =  [' + ', '.join(['{!r}'.format(v) for v in pt]) + '],\n'
         lines = lines + '    phi = [' + ', '.join(['{!r}'.format(v) for v in phi]) + '],\n'
+        lines = lines + '    met = {!r}'.format(met) + ',\n'
+        lines = lines + '    met_phi = {!r}'.format(met_phi) + ',\n'
         lines = lines + ')\n'
         lines = lines + '\n'
         lines = lines + 'altdphi_{} = {}\n'.format(njet_name, dumped)
