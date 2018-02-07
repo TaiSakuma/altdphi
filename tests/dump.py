898e90658188b29e76163c50d3536e5f1ebf0d54
Tai Sakuma <tai.sakuma@gmail.com>
2018-02-07 09:19:44 +0000

    add dump_float() and dump_special_np_float()

--- tests/dump.py
+++ tests/dump.py
@@ -25,3 +25,34 @@ def dump_altdphi(altdphi, target_class='mock.MagicMock'):
     return ret
 
 ##__________________________________________________________________||
+def dump_float(v):
+    module_name = type(v).__module__
+    class_name = v.__class__.__name__
+    if module_name == '__builtin__':
+        return "{}('{!r}')".format(class_name, v)
+
+    if module_name == np.__name__:
+        module_name = 'np'
+
+    if module_name == 'np':
+        special = dump_special_np_float(v)
+        if special is not None:
+            return special
+
+    return "{}.{}('{!r}')".format(module_name, class_name, v)
+
+##__________________________________________________________________||
+def dump_special_np_float(v):
+    specials =(
+        'np.float64(np.pi)',
+        'np.float64(np.pi/2)',
+        'np.float64(np.pi/4)',
+    )
+
+    for s in specials:
+        if v == eval(s):
+            return s
+
+    return None
+
+##__________________________________________________________________||
