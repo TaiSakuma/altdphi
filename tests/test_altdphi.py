c8039ae4d1f65a32e3c1704c2835df0c80f4e758
Tai Sakuma <tai.sakuma@gmail.com>
2018-02-07 09:46:36 +0000

    clean up code

--- tests/test_altdphi.py
+++ tests/test_altdphi.py
@@ -26,16 +26,16 @@ def event_altdphi(request):
 def test_altdphi(event_altdphi):
     event = event_altdphi[0]
     expected_altdphi = event_altdphi[1]
-    pt = np.array(event['jet_pt'])
-    phi = np.array(event['jet_phi'])
+    pt = event['jet_pt']
+    phi = event['jet_phi']
     actual_altdphi = AltDphi(pt=pt, phi=phi)
     assert_altdphi_equal(expected_altdphi, actual_altdphi)
 
 def test_altdphi_met(event_altdphi):
     event = event_altdphi[0]
     expected_altdphi = event_altdphi[2]
-    pt = np.array(event['jet_pt'])
-    phi = np.array(event['jet_phi'])
+    pt = event['jet_pt']
+    phi = event['jet_phi']
     met = event['met']
     met_phi = event['met_phi']
     actual_altdphi = AltDphi(pt=pt, phi=phi, mht=met, mht_phi=met_phi)
@@ -44,8 +44,8 @@ def test_altdphi_met(event_altdphi):
 ##__________________________________________________________________||
 def test_altdphi_monojet_is_minus_mht():
     event = event_monojet
-    pt = np.array(event['jet_pt'])
-    phi = np.array(event['jet_phi'])
+    pt = event['jet_pt']
+    phi = event['jet_phi']
     altdphi = AltDphi(pt=pt, phi=phi)
     assert pt[0] == altdphi.mht
     assert [1] == altdphi.f
@@ -53,8 +53,8 @@ def test_altdphi_monojet_is_minus_mht():
 
 def test_altdphi_monojet_is_not_minus_mht():
     event = event_monojet
-    pt = np.array(event['jet_pt'])
-    phi = np.array(event['jet_phi'])
+    pt = event['jet_pt']
+    phi = event['jet_phi']
     mht = event['met']
     mht_phi = event['met_phi']
     altdphi = AltDphi(pt=pt, phi=phi, mht=mht, mht_phi=mht_phi)
