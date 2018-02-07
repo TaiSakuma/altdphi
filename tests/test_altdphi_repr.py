3997e737f08734c754ac3f9ea97a0a425fe390a8
Tai Sakuma <tai.sakuma@gmail.com>
2018-02-07 12:26:29 +0000

    update test data in test_altdphi_repr.py

--- tests/test_altdphi_repr.py
+++ tests/test_altdphi_repr.py
@@ -7,8 +7,8 @@ from altdphi import AltDphi
 
 ##__________________________________________________________________||
 def test_repr_without_mht():
-    pt = np.array([741.63, 498.69, 45.62])
-    phi = np.array([-1.41, 1.81, 0.92])
+    pt =  np.array([1149.60, 794.66, 251.60, 94.48])
+    phi = np.array([-1.32, 2.75, 0.87, -2.77])
     alt = AltDphi(pt=pt, phi=phi)
 
     # e.g.,
@@ -22,11 +22,12 @@ def test_repr_without_mht():
     assert repr(alt) + ':' == alt.to_string(all = True).split('\n')[0]
 
 def test_repr_with_mht():
-    pt = np.array([741.63, 498.69, 45.62])
-    phi = np.array([-1.41, 1.81, 0.92])
-    met = 264.16
-    met_phi = 1.44
+    pt =  np.array([1149.60, 794.66, 251.60, 94.48])
+    phi = np.array([-1.32, 2.75, 0.87, -2.77])
+    met = 783.54
+    met_phi = 1.019
     alt = AltDphi(pt=pt, phi=phi, mht=met, mht_phi=met_phi)
+
     # e.g.,
     # 'AltDphi(pt=array([ 741.63,  498.69,   45.62]), phi=array([-1.41,  1.81,  0.92]), mht=264.16, mht_phi=1.44)'
     assert 'pt' in repr(alt)
