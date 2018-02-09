8a6ac626269f9e97d193b2dd0d807b565d036695
Tai Sakuma <tai.sakuma@gmail.com>
2018-02-09 18:59:41 +0000

    disable sidebarrel

--- docs/conf.py
+++ docs/conf.py
@@ -115,6 +115,7 @@ html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
 # html_theme_options = {}
 html_theme_options = {
     'navbar_site_name': "Site",
+    'navbar_sidebarrel': False,
     'navbar_pagenav': False,
     'navbar_links': [
         ('<i class="fab fa-github fa-lg"></i>', "https://github.com/TaiSakuma/altdphi", True),
